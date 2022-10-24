a023aa599fca998566af9d58f37361e6de3e2ed6
gaby gramajo <ggabrieldeveloper@gmail.com>
2022-10-24 12:36:43 -0300

    class 13 - addEventListener

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const h1 = document.querySelector('h1');
+const form = document.querySelector("#form");
 const input1 = document.querySelector("#calculo1");
 const input2 = document.querySelector("#calculo2");
 const btn = document.querySelector("#btnCalcular");
 const pResult = document.querySelector("#result"); 
 
 
+btn.addEventListener("click", btnOnClick);
+
 function btnOnClick() {
   const sumaInputs = +input1.value + +input2.value;
   pResult.innerHTML = "Resultado: " + sumaInputs;
 }
+
+// form.addEventListener("submit", sumarInputValues);
+
+// function sumarInputValues(event) {
+
+//   event.preventDefault(); // evita la recarga 
+  
+//   const sumaInputs = +input1.value + +input2.value;
+//   pResult.innerHTML = "Resultado: " + sumaInputs;
+// }
